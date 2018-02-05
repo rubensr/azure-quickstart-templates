c3ba58ce86db48b40ab47f4552c1611d3487632f
Rubens Ramos <rubens_ramos@yahoo.com>
2018-02-05 18:45:04 +1030

    Stripping back VMs

---
+++ sharepoint-adfs-dsc/setup/SetupDCVM.ps1
@@ -0,0 +1,38 @@
+Configuration SetupDCVM {
+    param(
+      [String]  $RegistrationKey       = "4826093e-3611-463c-bec4-571ea9f280ec",
+      [Int]     $DSCPort               = 8080,
+      [String]  $CertificateThumbprint = "1A01A0E90FD240C665A745718A1AAE08BFB99B82",
+      [Boolean] $DSCSelfSignedCerts    = $true
+    )
+    
+    Node localhost {
+        WindowsFeature DSCServiceFeature {
+            Ensure = "Present"
+            Name   = "DSC-Service"
+        }
+
+        xDscWebService PSDSCPullServer {
+            Ensure                          = "Present"
+            EndpointName                    = "PSDSCPullServer"
+            Port                            = $DSCPort
+            PhysicalPath                    = "$env:SystemDrive\inetpub\wwwroot\PSDSCPullServer"
+            ModulePath                      = "$env:PROGRAMFILES\WindowsPowershell\DscService\Modules"
+            ConfigurationPath               = "$env:PROGRAMFILES\WindowsPowershell\DscService\Configuration"
+            RegistrationKeyPath             = "$env:PROGRAMFILES\WindowsPowerShell\DscService"   
+            UseSecurityBestPractices        = $true
+            CertificateThumbprint           = $CertificateThumbprint
+            AcceptSelfSignedCertificates    = $DSCSelfSignedCerts
+            State                           = "Started"
+            DependsOn                       = "[WindowsFeature]DSCServiceFeature"
+        }
+
+        File RegistrationKeyFile
+        {
+            Ensure          = 'Present'
+            Type            = 'File'
+            DestinationPath = "$env:ProgramFiles\WindowsPowerShell\DscService\RegistrationKeys.txt"
+            Contents        = $RegistrationKey
+        }
+    }
+}
