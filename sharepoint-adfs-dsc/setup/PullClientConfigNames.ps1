147f80ced5bc993a90d41deac3c1cb0fcbb7cd88
Rubens Ramos <rubens_ramos@yahoo.com>
2018-02-06 11:48:30 +1030

    more scripts

---
+++ sharepoint-adfs-dsc/setup/PullClientConfigNames.ps1
@@ -0,0 +1,31 @@
+[DSCLocalConfigurationManager()]
+configuration PullClientConfigNames
+{
+    Node sp
+    {
+        Settings
+        {
+            CertificateID        = "95B14F52F627E44E974C23AFCEF6CBB5F8411552"
+            RefreshMode          = 'Pull'
+            RefreshFrequencyMins = 30
+            RebootNodeIfNeeded   = $true
+        }
+
+        ConfigurationRepositoryWeb CONTOSO-PullSrv
+        {
+            ServerURL          = 'https://dsc.contoso.local:8080/psdscpullserver.svc'
+            RegistrationKey    = '4826093e-3611-463c-bec4-571ea9f280ec'
+            AllowUnsecureConnection = $false
+            ConfigurationNames = @('SPWFEConfig')
+        }   
+
+        ReportServerWeb CONTOSO-PullSrv
+        {
+            ServerURL       = 'https://dsc.contoso.local:8080/psdscpullserver.svc'
+            RegistrationKey = '4826093e-3611-463c-bec4-571ea9f280ec'
+            AllowUnsecureConnection = $false
+        }
+    }
+}
+
+PullClientConfigNames
---
+++ sharepoint-adfs-dsc/setup/SPWFE.ps1
@@ -0,0 +1,35 @@
+Configuration SPWFE {
+  Param (
+      [Parameter(Mandatory=$true)]
+      [ValidateNotNullOrEmpty()]
+      [pscredential]
+      $Credential
+  )
+  Import-DscResource -ModuleName xTimeZone, PSDesiredStateConfiguration
+
+  Node SPWFEConfig {
+      Group TestGroup {
+          GroupName = "TestGroup"
+          Ensure = "Present"
+          Credential = $Credential
+      }
+
+      xTimeZone TimeZoneExample {
+          TimeZone = "Cen. Australia Standard Time" 
+          #TimeZone = "Eastern Standard Time"
+          IsSingleInstance = "Yes"
+      }
+  }
+}
+
+$cd = @{
+  AllNodes = @(    
+      @{ 
+          NodeName = "SPWFEConfig"
+          CertificateFile = "F:\setup\dscencrypt.cer"
+      }
+  ) 
+}
+
+General -OutputPath 'f:\configs\SPWFE' -ConfigurationData $cd -Credential (Get-Credential -Message 'Enter Credential for configuration')
+New-DscChecksum -Path 'f:\configs\SPWFE\SPWFEConfig.mof'
